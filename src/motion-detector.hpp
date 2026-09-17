1074bad15ba5c22d0fec0437d622da99aca9a26c
yuuki-ama1015 <yuuki.am3192@gmail.com>
2026-09-17 18:43:01 +0900

    Add monitor region settings to MotionDetector

--- src/motion-detector.hpp
+++ src/motion-detector.hpp
@@ -34,6 +34,13 @@ public:
 	void setMinimumRecordingDuration(seconds duration);
 	seconds minimumRecordingDuration() const { return min_recording_duration_; }
 
+	// Monitor only a percentage region of the analyzed frame.
+	// When disabled, the full frame is used. Values are clamped to 0-100.
+	void setRegionEnabled(bool enabled);
+	bool isRegionEnabled() const { return region_enabled_; }
+	void setRegionPercent(int x, int y, int w, int h);
+	void regionPercent(int &x, int &y, int &w, int &h) const;
+
 	bool isMonitoring() const { return monitoring_; }
 	double lastMotionPercent() const { return last_motion_percent_; }
 	seconds stillnessDuration() const;
@@ -52,6 +59,10 @@ private:
 	void ensureCallbackRegistered();
 	void ensureCallbackRemoved();
 	void resetStillness();
+	void clearPreviousFrame();
+	void clampRegion();
+	void resolveRegionPixels(uint32_t frameW, uint32_t frameH, uint32_t &rx,
+				 uint32_t &ry, uint32_t &rw, uint32_t &rh) const;
 	static double meanAbsDiffPercent(const uint8_t *a, const uint8_t *b,
 					 size_t count);
 
@@ -60,12 +71,20 @@ private:
 	double sensitivity_percent_{2.0};
 	seconds min_recording_duration_{5 * 60};
 
+	bool region_enabled_ = false;
+	int region_x_percent_ = 0;
+	int region_y_percent_ = 0;
+	int region_w_percent_ = 100;
+	int region_h_percent_ = 100;
+
 	bool monitoring_ = false;
 	bool callback_registered_ = false;
 
 	std::vector<uint8_t> prev_luma_;
-	uint32_t frame_width_ = 0;
-	uint32_t frame_height_ = 0;
+	uint32_t analyze_width_ = 0;
+	uint32_t analyze_height_ = 0;
+	uint32_t region_pixel_w_ = 0;
+	uint32_t region_pixel_h_ = 0;
 
 	double last_motion_percent_ = 0.0;
 	clock::time_point stillness_started_{};
