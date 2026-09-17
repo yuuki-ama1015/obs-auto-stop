3da2d3e1c8f88e53ed2ced9a3879da29c2a5a530
yuuki-ama1015 <yuuki.am3192@gmail.com>
2026-09-17 17:40:34 +0900

    Add AutoStopDock widget for OBS dock UI

--- src/auto-stop-dock.hpp
+++ src/auto-stop-dock.hpp
@@ -1,6 +1,39 @@
 #pragma once
 
-class AutoStopDock {
+#include <QWidget>
+
+class QCheckBox;
+class QSpinBox;
+class QLabel;
+class QTimer;
+
+class RecordingMonitor;
+class StopController;
+
+// OBS Dock UI for OBS Auto Stop (max recording timer + status).
+class AutoStopDock : public QWidget {
+	Q_OBJECT
+
 public:
-    AutoStopDock() = default;
+	AutoStopDock(RecordingMonitor *monitor, StopController *stop,
+		     QWidget *parent = nullptr);
+	~AutoStopDock() override;
+
+private slots:
+	void onAutoStopToggled(bool enabled);
+	void onMaxMinutesChanged(int minutes);
+	void refreshStatus();
+
+private:
+	void loadSettings();
+	void saveSettings() const;
+
+	RecordingMonitor *monitor_ = nullptr;
+	StopController *stop_ = nullptr;
+
+	QCheckBox *autoStopCheck_ = nullptr;
+	QSpinBox *maxMinutesSpin_ = nullptr;
+	QLabel *statusLabel_ = nullptr;
+	QLabel *elapsedLabel_ = nullptr;
+	QTimer *refreshTimer_ = nullptr;
 };
