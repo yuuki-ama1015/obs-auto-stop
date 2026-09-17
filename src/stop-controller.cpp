1fd023c7aeff5970c5befd8fcb740c5ce2c4ef01
yuuki-ama1015 <yuuki.am3192@gmail.com>
2026-09-17 17:05:59 +0900

    Implement StopController via obs_frontend_recording_stop

--- src/stop-controller.cpp
+++ src/stop-controller.cpp
@@ -1,1 +1,19 @@
 #include "stop-controller.hpp"
+
+#include <obs-frontend-api.h>
+#include <obs-module.h>
+
+void StopController::requestStop(const char *reason)
+{
+	if (stop_requested_) {
+		return;
+	}
+
+	stop_requested_ = true;
+	blog(LOG_INFO, "OBS Auto Stop: requesting recording stop (%s)",
+	     reason ? reason : "unspecified");
+
+	if (obs_frontend_recording_active()) {
+		obs_frontend_recording_stop();
+	}
+}
