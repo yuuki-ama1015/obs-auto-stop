7a93c806aca63d2d7d461c6b8f93da947383cb78
yuuki-ama1015 <yuuki.am3192@gmail.com>
2026-09-17 18:11:49 +0900

    Expose motion settings in AutoStopDock

--- src/auto-stop-dock.hpp
+++ src/auto-stop-dock.hpp
@@ -4,24 +4,29 @@
 
 class QCheckBox;
 class QSpinBox;
+class QDoubleSpinBox;
 class QLabel;
 class QTimer;
 
 class RecordingMonitor;
+class MotionDetector;
 class StopController;
 
-// OBS Dock UI for OBS Auto Stop (max recording timer + status).
 class AutoStopDock : public QWidget {
 	Q_OBJECT
 
 public:
-	AutoStopDock(RecordingMonitor *monitor, StopController *stop,
-		     QWidget *parent = nullptr);
+	AutoStopDock(RecordingMonitor *monitor, MotionDetector *motion,
+		     StopController *stop, QWidget *parent = nullptr);
 	~AutoStopDock() override;
 
 private slots:
 	void onAutoStopToggled(bool enabled);
 	void onMaxMinutesChanged(int minutes);
+	void onMotionToggled(bool enabled);
+	void onInactivitySecondsChanged(int seconds);
+	void onSensitivityChanged(double percent);
+	void onMinRecordingMinutesChanged(int minutes);
 	void refreshStatus();
 
 private:
@@ -29,11 +34,17 @@ private:
 	void saveSettings() const;
 
 	RecordingMonitor *monitor_ = nullptr;
+	MotionDetector *motion_ = nullptr;
 	StopController *stop_ = nullptr;
 
 	QCheckBox *autoStopCheck_ = nullptr;
 	QSpinBox *maxMinutesSpin_ = nullptr;
+	QCheckBox *motionCheck_ = nullptr;
+	QSpinBox *inactivitySpin_ = nullptr;
+	QDoubleSpinBox *sensitivitySpin_ = nullptr;
+	QSpinBox *minRecordingSpin_ = nullptr;
 	QLabel *statusLabel_ = nullptr;
 	QLabel *elapsedLabel_ = nullptr;
+	QLabel *motionLabel_ = nullptr;
 	QTimer *refreshTimer_ = nullptr;
 };
